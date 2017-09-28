a632cebcae9f430da3ea3f89d530599da60e75dd
madacoo <mdcooper1705@gmail.com>
2017-09-28 11:19:03 +0800

    Vector.prototype.distance
    Use Math.hypot to determine distance between vectors.

--- vector.js
+++ vector.js
@@ -3,6 +3,7 @@ function Vector(x, y) {
     this.y = y;
 }
 
+
 Vector.prototype.add = function(v) {
     /* Add another vector to this one. */
     this.x += v.x;
@@ -10,17 +11,20 @@ Vector.prototype.add = function(v) {
     return this;
 };
 
-Vector.prototype.sub = function(v) {
-    /* Subtract another vector from this one. */
-    this.x -= v.x;
-    this.y -= v.y;
-    return this;
-};
 
 Vector.prototype.copy = function() {
     /* Return a copy of this vector */
     return new Vector(this.x, this.y);
 };
+
+
+Vector.prototype.distance = function(v) {
+	/* Return the distance between this and v. */
+	let dX = this.x - v.x;
+	let dY = this.y - v.y;
+	return Math.hypot(dX, dY);
+}
+
 
 Vector.prototype.rotate = function(theta) {
 	/* Rotate this Vector around origin by theta. */
@@ -31,4 +35,12 @@ Vector.prototype.rotate = function(theta) {
 
     this.x = Math.floor(x * cosTheta - y * sinTheta);
     this.y = Math.floor(x * sinTheta + y * cosTheta);
+};
+
+
+Vector.prototype.sub = function(v) {
+    /* Subtract another vector from this one. */
+    this.x -= v.x;
+    this.y -= v.y;
+    return this;
 };
