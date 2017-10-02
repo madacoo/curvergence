fff7ce6e28621a37ea414607462be99382281093
madacoo <mdcooper1705@gmail.com>
2017-10-02 15:12:41 +0800

    fixed bug where palette was returning invalid hex colours

--- palette.js
+++ palette.js
@@ -2,21 +2,22 @@ function Palette(r, g, b) {
     this.r = r || this.randomPrimary();
     this.g = g || this.randomPrimary();
     this.b = b || this.randomPrimary();
-    
+
 }
 
-Palette.prototype.randomPrimary = function() {
-	return Math.floor(Math.random()*257);
+Palette.prototype.randomPrimary = function(mixer) {
+    mixer = parseInt(mixer, 16) || 127;
+    let rnd = Math.random()*256;
+    let val = Math.floor((rnd + mixer)/2);
+    let hex = val.toString(16);
+    if (hex.length == 2) return hex;
+    return "0" + hex;
+
 };
 
 Palette.prototype.getColor = function() {
-	let r = this.randomPrimary();
-	let g = this.randomPrimary();
-	let b = this.randomPrimary();
-	r = Math.floor((r + this.r)/2);
-	g = Math.floor((g + this.g)/2);
-	b = Math.floor((b + this.b)/2);
-    
-    return "#" + r.toString(16) + g.toString(16) + b.toString(16);
-
+	let r = this.randomPrimary(this.r);
+	let g = this.randomPrimary(this.g);
+	let b = this.randomPrimary(this.b);
+    return "#" + r + g + b;
 };
